b1090bf78c1b58f91e263954cf54790e1d23bf96
Pereverzev Marat <marat.pereverzrv@gmail.com>
2023-09-13 18:40:53 +0300

    refactor: syntax 48.sh

--- 48.sh
+++ 48.sh
@@ -2,45 +2,33 @@ read -p "Input any number: " number
 read -rp "Input a path: " path
 
 number=$(echo $number | sed 's/^0*//')
-
 isPassed=1
-
 checkNumber() {
-	if [ "$number" == "" ] 
-	then
+	if [ "$number" == "" ]; then
 		isPassed=0
 		echo "the 'number' variable isn't defined"
-	elif ! [[ $number =~ ^-?[0-9]+$ ]]
-		then
-			isPassed=0
-			echo "the 'number' variable doesn't have number type"
+	elif ! [[ $number =~ ^-?[0-9]+$ ]]; then
+		isPassed=0
+		echo "the 'number' variable doesn't have number type"
 	fi
 }
-
 checkPath() {
-	if [ "$path" == "" ]
-	then
+	if [ "$path" == "" ]; then
 		isPassed=0
 		echo "the 'path' variable isn't defined"
-	elif ! [ -e $path ] 
-		then
-			isPassed=0
-			echo "the entered path doesn't exist"
+	elif ! [ -e $path ]; then
+		isPassed=0
+		echo "the entered path doesn't exist"
 	fi
 }
-
 checkNumber
 checkPath
-
-if [ $isPassed -eq 1 ]
-then
+if [ $isPassed -eq 1 ]; then
 	fileName="$(printf "%(%d.%m.%Y)T").txt"
-
 	if [ -e $fileName ]; then
    		rm "$fileName"
 	fi
 	touch "$fileName"
-
 	cat $path | while read line 
 	do
 	   read -a data <<< "$line"
@@ -50,3 +38,5 @@ then
 	   fi
 	done
 fi
+
+
