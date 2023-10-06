3734d3cc0e2724a1bcdb8f970392b7c845bfcd90
Daniil Semenov <66639629+danissimoff@users.noreply.github.com>
2023-10-06 21:46:06 +0300

    Update laba1.bat

--- laba1.bat
+++ laba1.bat
@@ -1,22 +1,18 @@
 @echo off
 set /p input="Введите путь к папке: "
-if exist "%input%" (
-	echo да есть
-)	else (
+
+if not exist "%input%" (
 	echo такой папки нет
 	timeout 3
 	exit
 )
-if exist "%input%\files" (
-	echo все четко
-)	else (
-	mkdir "%input%/files"
-)
+
+if not exist "%input%\files" mkdir "%input%\files"
 
 for /r "%input%" %%f in (*) do (
 	if %%~zf GTR 1048576 (
 	move "%%f" "%input%/files"
 	echo перемещён файл "%%f"
-)
+	)
 )
 pause
