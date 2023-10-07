68c1c459a9efe19d472976d2a0de12cd99d34e8a
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2023-10-07 18:59:21 +0300

    Add files via upload

---
+++ pioivis.sh
@@ -0,0 +1,10 @@
+touch {first.txt,second.txt}
+
+i=0
+while [ $i -le $(( RANDOM )) ]
+do
+echo $[ $RANDOM % 899 + 100 ]>> first.txt
+((i++))
+done
+grep -i "23" first.txt >> second.txt
+sort -r second.txt
---
+++ pioivis_lr1_losik.bat
@@ -0,0 +1,20 @@
+@echo off
+cd Desktop
+start first.txt
+start second.txt
+taskkill /im notepad.exe
+set /a _rand = (%random%*100/32767)+1
+set /a i=0
+:loop
+if %i%==%_rand% goto end
+set /a r = 200+(343-200+1)*%random%/32768
+echo %r% >> C:\Users\yaroslave\Desktop\first.txt
+set /a i=i+1
+goto loop
+:end
+findstr /r /c:"23" first.txt >> second.txt
+cd Desktop
+sort /r second.txt /o second.txt
+start second.txt
+start first.txt 
+pause
