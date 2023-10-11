deeee9b8c1bd5acd75165bfbb3d4135dd52d2ffa
Randal <neborsky.ivan@tut.by>
2023-10-11 22:12:50 +0300

    edited README and added folder with images

--- Lab/folder with scripts/BashScript.sh
+++ Lab/folder with scripts/BashScript.sh
@@ -1,7 +1,9 @@
- read N
- read path
+echo Enter amount of letters
+read Amount_Of_Letters 
+echo Enter path
+read path
 
- grep -h -E -i "^(.* )?[а-яёa-z]{$N}( .*)?$" $path*.txt > result.txt
+grep -h -E -i "^(.* )?[а-яёa-z]{$Amount_Of_Letters}( .*)?$" $path*.txt > result.txt
 
- echo Press Enter to continue
- read randomVar
+echo Press Enter to continue
+read randomVar
