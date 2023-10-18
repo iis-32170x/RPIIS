2d82e83251d60f8f7c3408b59fa0f00a360b7051
user_KLimkov <maratklimkov2208@gmail.com>
2023-10-18 12:10:16 +0300

    added files

---
+++ ПОИИС1/LABAn1BAT.bat
@@ -0,0 +1,13 @@
+@echo off
+if exist "C:\PiOIvIS" (
+chcp 1251>nul
+echo Atrib=RA: >>result.txt
+dir /b/a:r >>result.txt
+echo Atrib=HA: >>result.txt
+dir /b/a:h >>result.txt
+echo Atrib=A: >>result.txt
+dir /b/a:-r-h >>result.txt
+) else (
+echo Dannoi papki net
+)
+pause
