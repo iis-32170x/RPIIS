c1681aad4a21a8b9110639e3a7ef5857eba08246
bachaa7 <144945398+bachaa7@users.noreply.github.com>
2023-10-18 14:21:56 +0300

    Update 41.sh

--- 41.sh
+++ 41.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+read -p "Введите количество процессов N: " N
+processes=$(top -b -n 1 -o %MEM | head -n "$((N+7))" | tail -n "$N")
+echo "Список top $N процессов:
+  "echo "$processes"
+max_mem_process=$(echo "$processes" | head -n 1 | awk '{ print $NF }')
+formatted_date=$(date +"%Y-%m-%d")
+filename="${max_mem_process}_${formatted_date}.txt"
+echo "$max_mem_process $formatted_date" > "$filename"
+echo "Создан новый файл: $filename"
 
