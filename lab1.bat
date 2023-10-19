42e5696de9745c02f99bfd0b30a0810e2885440c
vysotskyz <145581766+vysotskyz@users.noreply.github.com>
2023-10-19 22:02:30 +0300

    Create lab1.bat

---
+++ lab1.bat
@@ -0,0 +1,29 @@
+@chcp 1251
+@echo off
+
+set /p divisor=Введите число, на которое необходимо разделить PID:
+
+tasklist /nh /fo csv > tasks.txt
+
+setlocal enabledelayedexpansion
+
+set /a count=0
+
+for /f "tokens=1,2 delims=," %%a in (tasks.txt) do (
+set /a pid=%%b
+set /a remainder=!pid! %% !divisor!
+if !remainder! equ 0 (
+echo process=
+echo %%a
+echo pid=
+echo %%b
+set /a count+=1
+)
+)
+timeout 30
+del tasks.txt
+
+if !count! equ 1 (
+echo процесс не найден. программа закроется через 3 сек
+timeout  3 
+)
