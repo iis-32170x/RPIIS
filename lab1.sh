7c5730eb90cd04ed22cfe4160e9e92a7679273e8
vysotskyz <145581766+vysotskyz@users.noreply.github.com>
2023-10-19 21:38:48 +0300

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,11 @@
+echo "Введите число:"
+read divisor
+pid_list=$(ps -e -o pid,comm --no-header | awk -v divisor="$divisor" '$1 % divisor == 0 {print $2, $1}')
+if [ -n "$pid_list" ]; then
+  echo "Процессы, PID которых делится нацело на $divisor:"
+  echo "$pid_list"
+else
+  echo "Нет процессов, PID которых делится нацело на $divisor."
+  sleep 3
+fi
+:set ff=unix
