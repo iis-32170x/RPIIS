e95a34c4a3117411d422e5d614b208ad43e57adb
RivaKing <147256759+RivaKing@users.noreply.github.com>
2023-10-22 14:18:47 +0300

    Добавлены файлы

---
+++ Лаба 84.bat
@@ -0,0 +1,21 @@
+@chcp 1251
+@echo off
+md d:\folder1
+md d:\folder2
+md d:\folder3
+md d:\tempfolder
+Echo %date%> D:\folder1\Text1.txt
+copy d:\folder1\Text1.txt d:\folder2
+ren d:\folder2\Text1.txt %date:~,2%.txt
+FOR %%a IN (%date:~,2%.txt) DO set strvar=%%a 
+ECHO %strvar%> d:\tempfolder\tempfile.txt
+FOR %%n IN (d:\tempfolder\tempfile.txt) DO ( SET /A strlength=%%~zn - 3 )
+Echo %strlength% >> D:\folder2\%date:~,2%.txt
+copy d:\folder2\%date:~,2%.txt d:\folder3
+ver >> D:\folder3\%date:~,2%.txt
+::ver > d:\tempfolder\tempfile1.txt
+::set bublick=d:\tempfolder\tempfile1.txt
+::For /F "usebackq tokens=* delims=" %%i In ("%bublick%") Do Set versia=%%i
+::Echo %versia% >> D:\folder3\%date:~,2%.txt
+RD /S /Q D:\tempfolder
+pause
