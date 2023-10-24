06e4ea8d5e23ee4d2a124a79d56c961a9486d16f
xarturhax <144942899+xarturhax@users.noreply.github.com>
2023-10-24 23:08:37 +0300

    Update sh1.sh

--- sh1.sh
+++ sh1.sh
@@ -1,11 +1,15 @@
-```bash
-# Получаем суммарный объем памяти всех активных процессов
-totalmemory=$(ps -eo rss= | awk '{sum += $1} END {print sum}')
+!/bin/bash
+
+# Получаем суммарный объем памяти активных процессов
+memory_usage=$(ps aux | awk '{ sum += $6 } END { print sum }')
 
 # Получаем число активных процессов
-numprocesses=$(ps -e | wc -l)
+process_count=$(ps -e | wc -l)
 
 # Выводим результаты в файл
-echo "Суммарный объем памяти: $totalmemory KB" > results.txt
-echo "Число активных процессов: $numprocesses" >> results.txt
-```
+echo "Суммарный объем памяти: $memory_usage" > memory_usage.txt
+echo "Число активных процессов: $process_count" >> memory_usage.txt
+
+# Выводим результаты на экран
+echo "Суммарный объем памяти: $memory_usage"
+echo "Число активных процессов: $process_count"
