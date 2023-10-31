9d79326c2a03123763e149df2adb4cebec56a6f1
artasik <48830085+SawaulPlay@users.noreply.github.com>
2023-11-01 00:07:20 +0300

    laba.sh

--- laba.sh
+++ laba.sh
@@ -1,1 +1,21 @@
+read -p ""  file1
+read -p "" file2
 
+string1=$(wc -l < $file1 | awk '{print $1}')
+string2=$(wc -l < $file2| awk '{print $1}')
+if [ $string1 -eq $string2 ]; then
+  echo "Количество строк одинаковое"
+else
+    echo "Количество строк разное"
+    word1=$(wc -w < $file1 | awk '{print $1}')
+    word2=$(wc -w < $file2 | awk '{print $1}')
+      if [ $word1 -eq $word2 ]; then
+        echo "Количество cлов одинаковое"
+      else
+        echo "Количество слов разное"
+      fi
+
+fi
+
+
+sleep 7
