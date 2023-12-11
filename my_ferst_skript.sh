c2c0c96a119bf730ce50729fee7be6f321baea0c
Kukrynitza <ilapolityko@gmail.com>
2023-12-11 17:22:33 +0300

    Delete sh

--- my_ferst_skript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
- folder_path="$1"
-
-if [ ! -d "$folder_path" ]; then  echo "Данной папки нет" 
-	exit 1 
-fi
-echo $folder_path
-find "$folder_path" -type f -name "*temp*" -exec head -n 5 {} \; > /home/ilja/.myskript/result
