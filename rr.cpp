8a44741642d8540d4f56e4de8fe00409043134b5
SharkoStepan <144940807+SharkoStepan@users.noreply.github.com>
2023-12-28 01:36:40 +0300

    Add files via upload

---
+++ rr.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+const int INF = INT_MAX;
+
+int radius(vector<vector<int>>& adjMatrix, int V) {
+    int rad = INF;
+
+    for (int i = 0; i < V; i++) {
+        vector<int> dist(V, INF);
+        dist[i] = 0;
+
+        queue<int> q;
+        q.push(i);
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int v = 0; v < V; v++) {
+                if (adjMatrix[u][v] != INF && dist[v] == INF) {
+                    dist[v] = dist[u] + adjMatrix[u][v];
+                    q.push(v);
+                }
+            }
+        }
+
+        int maxDist = 0;
+        for (int j = 0; j < V; j++) {
+            maxDist = max(maxDist, dist[j]);
+        }
+
+        rad = min(rad, maxDist);
+    }
+
+    return rad;
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int V;
+    cout << "Введите число вершин: ";
+    cin >> V;
+
+    vector<vector<int>> adjMatrix(V, vector<int>(V));
+    cout << "Введите матрицу смежности (если нет ребра, введите INF):" << endl;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> adjMatrix[i][j];
+        }
+    }
+
+    int rad = radius(adjMatrix, V);
+    cout << "Радиус взвешенного ориентированного графа: " << rad << endl;
+
+    return 0;
+}
