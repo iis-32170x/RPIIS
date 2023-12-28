2c5f7d455a7be4e83995849872aac2b1a5694a8f
viktoriaDEVV <pinkkorangge@gmail.com>
2023-12-28 03:04:13 +0300

    new matrix

---
+++ RR/Matrix_main/Matrix_main/Matrix_main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Функция для создания матрицы инцидентности из списка ребер
+vector<vector<int>> createIncidenceMatrix(const vector<vector<int>>& edges, int numVertices) {
+    vector<vector<int>> incidenceMatrix(numVertices, vector<int>(edges.size(), 0));
+    for (int i = 0; i < edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        incidenceMatrix[u][i] = 1;
+        incidenceMatrix[v][i] = 1;
+    }
+    return incidenceMatrix;
+}
+
+int main() {
+    vector<vector<int>> graph1 = {
+        {1, 1, 0},
+        {1, 0, 1},
+        {0, 1, 1}
+    };
+    vector<vector<int>> graph2 = {
+           {1, 0, 1},
+           {1, 0, 0},
+           {0, 1, 0},
+           {0, 1, 1}
+    };
+
+    int numVertices = 4; // Общее количество вершин в объединенном графе
+    vector<vector<int>> combinedEdges = graph1; // Добавляем ребра из первого графа
+    combinedEdges.insert(combinedEdges.end(), graph2.begin(), graph2.end()); // Добавляем ребра из второго графа
+
+    vector<vector<int>> combinedIncidenceMatrix = createIncidenceMatrix(combinedEdges, numVertices);
+
+    // Вывод матрицы инцидентности
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < combinedEdges.size(); j++) {
+            cout << combinedIncidenceMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
--- RR/matrix/matrix/matrix.cpp
+++ RR/matrix/matrix/matrix.cpp
@@ -1,51 +1,85 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
+// Структура для представления ребра
+struct Edge {
+    int numVertices, numEdges;
+};
+
 // Функция для объединения графов
-vector<vector<int>> unionGraphs(vector<vector<int>>& graph1, vector<vector<int>>& graph2) {
-    int n1 = graph1.size(); // Количество вершин в первом графе
-    int n2 = graph2.size(); // Количество вершин во втором графе
-    int m1 = graph1[0].size(); // Количество рёбер в первом графе
-    int m2 = graph2[0].size(); // Количество рёбер во втором графе
+/*vector<vector<int>> unionGraphs(const vector<vector<int>>& graph1, const vector<vector<int>>& graph2) {
+    int n1 = graph1.size();
+    int n2 = graph2.size(); 
+    int n = n1 + n2; 
 
-    // Создаем новую матрицу инцидентности для объединенного графа
-    vector<vector<int>> unionGraph(n1 + n2, vector<int>(m1 + m2, 0));
+    // Создаем матрицу инцидентности для объединенного графа
+    vector<vector<int>> incidenceMatrix(n, vector<int>(n1 + n2, 0));
 
-    // Копируем матрицу инцидентности первого графа в объединенный граф
+    // Заполня0ем матрицу инцидентности для первого графа
     for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m1; j++) {
-            unionGraph[i][j] = graph1[i][j];
+        for (int j : graph1[i]) {
+            incidenceMatrix[i][i] = 1;
+            incidenceMatrix[j][i] = 1;
         }
     }
 
-    // Копируем матрицу инцидентности второго графа в объединенный граф
+    // Заполняем матрицу инцидентности для второго графа
     for (int i = 0; i < n2; i++) {
-        for (int j = 0; j < m2; j++) {
-            unionGraph[n1 + i][m1 + j] = graph2[i][j];
+        for (int j : graph2[i]) {
+            incidenceMatrix[i + n1][i] = 1;
+            incidenceMatrix[j + n1][i] = 1;
         }
     }
 
-    return unionGraph;
+    return incidenceMatrix;*/
+vector<set<int>> uniteGraphs(const vector<set<int>>& graph1, const vector<set<int>>& graph2) {
+    int numVertices = max(graph1.size(), graph2.size());
+    vector<set<int>> unitedGraph(numVertices);
+
+    for (int i = 0; i < graph1.size(); i++) {
+        for (int vertex : graph1[i]) {
+            unitedGraph[i].insert(vertex);
+        }
+    }
+
+    for (int i = 0; i < graph2.size(); i++) {
+        for (int vertex : graph2[i]) {
+            unitedGraph[i].insert(vertex);
+        }
+    }
+
+    return unitedGraph;
 }
 
 int main() {
-    // Примеры матриц инцидентности для двух графов
-    vector<vector<int>> graph1 = { {1, 1, 0}, {1, 0, 1}, {0, 1, 1} };
-    vector<vector<int>> graph2 = { {1, 0}, {1, 1}, {0, 1} };
+    // Примеры графов
+    //vector<vector<int>> graph1 = { {1, 2}, {0, 2}, {0, 1} };
+   // vector<vector<int>> graph2 = { {1, 2}, {0, 1}, {0, 3}, {3,2} };
+    vector<set<int>> graph1 = {
+        {1, 1, 0},
+        {1, 0, 1},
+        {0, 1, 1}
+    };
+    vector<set<int>> graph2 = {
+        {1, 0, 1},
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 1, 1}
+    };
 
-    // Объединяем графы и выводим результат
-    vector<vector<int>> unionGraph = unionGraphs(graph1, graph2);
-    for (int i = 0; i < unionGraph.size(); i++) {
-        for (int j = 0; j < unionGraph[0].size(); j++) {
-            cout << unionGraph[i][j] << " ";
+
+    // Объединяем графы
+    vector<set<int>> unitedGraph = uniteGraphs(graph1, graph2);
+    // Выводим матрицу инцидентности объединенного графа
+    for (int i = 0; i < unitedGraph.size(); i++) {
+        for (int j = 0; j < unitedGraph.size(); j++) {
+            cout << unitedGraph[i][j] << " ";
         }
         cout << endl;
     }
 
     return 0;
 }
-
-
