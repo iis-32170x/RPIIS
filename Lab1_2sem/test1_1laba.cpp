56b0133ead0e58ff3bc2051700415a8b2f52426e
Dmitry Strunets <d.strunetss@gmail.com>
2024-01-30 14:43:15 +0300

    1

---
+++ Lab1_2sem/test1_1laba.cpp
@@ -0,0 +1,24 @@
+﻿#include "Tree.h"
+
+int main() {
+	
+	vector<vector<int>> matrix = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16}
+	};
+	QuadTree* root = NULL;
+	int size = matrix.size();
+	root = root-> TakeFromArray(matrix, 0, 0, size);
+	root->ViewTree(root, 0);
+	cout << endl;
+	vector<vector<int>> matrix1(4, vector<int>(4, 0));
+	root->TakeFromTree(root, matrix1, 0, 0, 4);
+	for (int i = 0; i < matrix1.size(); i++) {
+		for (int j = 0; j < matrix1[i].size(); j++) {
+			cout << matrix1[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
