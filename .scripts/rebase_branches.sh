41a61be5e9d6e40e9b9b2f8b3d2f416f9808d2ee
graffegiraffe <145375460+graffegiraffe@users.noreply.github.com>
2024-02-10 17:57:05 +0300

    Create rebase_branches.sh

---
+++ .scripts/rebase_branches.sh
@@ -0,0 +1,14 @@
+# author: https://github.com/danilyanich
+
+set -euxa pipefail
+
+branches=$(cat branches)
+
+echo "$branches" | while read branch
+do
+  git checkout "$branch"
+  git pull --rebase origin main
+  git push origin "$branch" --force-with-lease
+done
+
+git checkout main
