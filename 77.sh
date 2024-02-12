5f00c12c0926f487fdd3ec26e5aace7cb2445d1e
Oddchew <144374775+Oddchew@users.noreply.github.com>
2024-02-12 23:53:15 +0300

    Delete 77.sh

--- 77.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "Введите путь к папке: " pyt
-read -p "Введите имя: " imya
-ls $pyt/*.txt | awk -F "/" '{print $NF}' | sort > C:/test/$imya.txt
-echo "New file has been created"
-read -p "Нажмите любую клавишу для продолжения..."
