76c093fcd6c13b3cbe07d35a5d59290975c2e0ae
Астахов Артём <artemastahov27@gmail.com>
2024-02-22 21:01:15 +0300

    Добавил функцию удаления вершины

--- sem2/lab1/GraphLibrary/src/com/GraphLibrary/Graph.java
+++ sem2/lab1/GraphLibrary/src/com/GraphLibrary/Graph.java
@@ -1,13 +1,12 @@
 package com.GraphLibrary;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.Queue;
 
 public class Graph {
     ArrayList<ArrayList<String>> adjacency;
     Hashtable<String, Integer> indexes;
+    int countOfTable = 0;
 
     public Graph() {
         adjacency = new ArrayList<ArrayList<String>>();
@@ -16,7 +15,9 @@ public class Graph {
         adjacency.get(0).add("Main");
 
         indexes = new Hashtable<>();
-        indexes.put("Main", 0);
+        indexes.put("Main", countOfTable);
+
+        countOfTable++;
     }
 
     public Graph(String name) {
@@ -26,18 +27,23 @@ public class Graph {
         adjacency.get(0).add(name);
 
         indexes = new Hashtable<>();
-        indexes.put(name, 0);
+        indexes.put(name, countOfTable);
+
+        countOfTable++;
     }
 
     public void addVertex(String name) {
         adjacency.add(new ArrayList<String>());
         adjacency.get(adjacency.size() - 1).add(name);
 
-        indexes.put(name, adjacency.size() - 1);
+        indexes.put(name, countOfTable);
+
+        countOfTable++;
     }
 
     public void addEdge(String first, String second) {
         adjacency.get(indexes.get(first)).add(second);
+        adjacency.get(indexes.get(second)).add(first);
     }
 
     public void printAdjacencyList() {
@@ -52,22 +58,15 @@ public class Graph {
         }
     }
 
-    public Queue<String> addNeighbours(String nameOfActuallyVertex, Queue<String> before, ArrayList<String> protect) {
+    public void deleteVertex(String nameOfVertex) {
+        int index = indexes.get(nameOfVertex);
 
-        for (int i = 1; i < adjacency.get((indexes.get(nameOfActuallyVertex))).size(); i++) {
-            if (protect.contains(adjacency.get((indexes.get(nameOfActuallyVertex))).get(i)))
-                continue;
-            before.add(adjacency.get((indexes.get(nameOfActuallyVertex))).get(i));
+        for (int i = 1; i < adjacency.get(index).size(); i++) {
+            int tempIndex = indexes.get(adjacency.get(index).get(i));
+
+            adjacency.get(tempIndex).remove(nameOfVertex);
         }
 
-        return before;
-    }
-
-    public ArrayDeque<String> getFirstElement() {
-        ArrayDeque<String> q = new ArrayDeque<>();
-
-        q.add(adjacency.get(0).get(0));
-
-        return q;
+        adjacency.remove(index);
     }
 }
