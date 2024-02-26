05d759b8cdd8c4123616180a98f48acfec9e9720
oskolky <147048129+oskolky@users.noreply.github.com>
2024-02-26 18:42:28 +0300

    Update NG.h

--- sem2/lab1/NG.h
+++ sem2/lab1/NG.h
@@ -7,17 +7,15 @@
 class Ngraph {
 public:
 
-	std::map<int, std::set<int>> graph;
-    void AddVertex(int a);
-    void AddEdge(int a, int b);
+	
+   	void AddVertex(int a);
+	void AddEdge(int a, int b);
 	void DeleteVertex(int a);
-	void printGraph();
 	void DeleteEdge(int a, int b);
 	void DFSTree(int first);
 
 private:
-
+	std::map<int, std::set<int>> graph;
 	void DFS(int a, std::map<int, bool>& visited);
 	
-
 };
