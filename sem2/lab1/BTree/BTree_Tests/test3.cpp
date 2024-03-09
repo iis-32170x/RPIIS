99427ae354a96162c7b5a2720a975fb32d62479f
yyyyyano <145003765+yyyyyano@users.noreply.github.com>
2024-03-09 20:30:54 +0300

    Update and rename sem2/lab1/BTree/test.cpp to sem2/lab1/BTree/BTree_Tests/test3.cpp

--- sem2/lab1/BTree/BTree_Tests/test3.cpp
+++ sem2/lab1/BTree/BTree_Tests/test3.cpp
@@ -2,7 +2,6 @@
 #include "gtest/gtest.h" 
 #include "BTREE.hpp" 
 
-// Тестирование функции поиска в B-дереве
 TEST(BTreeTest, SearchTest) {
     BTree tree(5);
 
@@ -11,22 +10,21 @@ TEST(BTreeTest, SearchTest) {
     tree.insert(28);
     tree.insert(14);
 
-    ASSERT_TRUE(tree.search(7) == nullptr); // Ожидаем, что элемент 7 НЕ найден
+    ASSERT_TRUE(tree.search(7) == nullptr);
     tree.insert(7);
-    ASSERT_TRUE(tree.search(7) != nullptr); // Ожидаем, что элемент 7 найден
+    ASSERT_TRUE(tree.search(7) != nullptr);
 
     tree.insert(10);
     tree.insert(15);
     tree.insert(22);
     tree.insert(18);
 
-    ASSERT_TRUE(tree.search(15) != nullptr); // Ожидаем, что элемент 15 найден
-    ASSERT_TRUE(tree.search(20) == nullptr); // Ожидаем, что элемент 20 не найден
-    ASSERT_TRUE(tree.search(22) != nullptr);// Ожидаем, что элемент 22 найден
-    ASSERT_TRUE(tree.search(5) == nullptr);// Ожидаем, что элемент 5 не найден
+    ASSERT_TRUE(tree.search(15) != nullptr); 
+    ASSERT_TRUE(tree.search(20) == nullptr); 
+    ASSERT_TRUE(tree.search(22) != nullptr);
+    ASSERT_TRUE(tree.search(5) == nullptr);
 }
 
-// Тестирование функции вставки в B-дерево
 TEST(BTreeTest, InsertTest) {
     BTree tree(5);
 
@@ -45,7 +43,6 @@ TEST(BTreeTest, InsertTest) {
     tree.insert(28);
     tree.insert(14);
 
-    // Проверяем, что элементы действительно вставлены
     ASSERT_TRUE(tree.search(14) != nullptr);
     ASSERT_TRUE(tree.search(10) != nullptr);
     ASSERT_TRUE(tree.search(15) != nullptr);
@@ -55,7 +52,6 @@ TEST(BTreeTest, InsertTest) {
     ASSERT_TRUE(tree.search(28) != nullptr);
 }
 
-// Тестирование функции удаления из B-дерева
 TEST(BTreeTest, RemoveTest) {
     BTree tree(5);
 
@@ -74,29 +70,19 @@ TEST(BTreeTest, RemoveTest) {
     tree.insert(28);
     tree.insert(14);
 
-    //Проверяем, есть ли элемент 15 в дереве
     ASSERT_TRUE(tree.search(15) != nullptr);
-    // Удаляем элемент из дерева
     tree.remove(15);
-    // Проверяем, что элемент действительно удален
     ASSERT_TRUE(tree.search(15) == nullptr);
 
-    //Проверяем, есть ли элемент 14 в дереве
     ASSERT_TRUE(tree.search(14) != nullptr);
-    // Удаляем элемент из дерева
     tree.remove(14);
-    // Проверяем, что элемент действительно удален
     ASSERT_TRUE(tree.search(14) == nullptr);
 
-    /*Проверяем, есть ли элемент 17 в дереве*/
     ASSERT_TRUE(tree.search(17) != nullptr);
-  /*   Удаляем элемент из дерева*/
     tree.remove(17);
-    /* Проверяем, что элемент действительно удален*/
     ASSERT_TRUE(tree.search(17) == nullptr);
 }
 
-// Запуск всех тестов
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
