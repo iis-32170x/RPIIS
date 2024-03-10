0af0a3fce7d5111a4f23eaae8d7efc0178d8f4c5
Renat <renatdvdv@gmail.com>
2024-03-11 00:46:26 +0300

    fixed test

--- sem2/lab1/test.cpp
+++ sem2/lab1/test.cpp
@@ -3,7 +3,7 @@
 #include "../priorityQueue/priorityQueue.h"
 using namespace std;
 
-//“ест на вставку и извлечение
+
 TEST(PriorityQueueTest, InsertAndExtract1) {
     PriorityQueue queue;
 
@@ -11,72 +11,81 @@ TEST(PriorityQueueTest, InsertAndExtract1) {
 
     queue.insert(10);
     queue.insert(5);
-    queue.insert(20);
+    queue.insert(15);
+    queue.insert(2);
 
     EXPECT_FALSE(queue.isEmpty());
-    EXPECT_EQ(queue.extract(), 5);
+    EXPECT_EQ(queue.extract(), 15);
     EXPECT_EQ(queue.extract(), 10);
+    EXPECT_EQ(queue.extract(), 5);
+    EXPECT_EQ(queue.extract(), 2);
+    EXPECT_TRUE(queue.isEmpty());
+}
+
+
+TEST(PriorityQueueTest, InsertAndExtract2) {
+    PriorityQueue queue;
+
+    queue.insert(10);
+    queue.insert(5);
+    queue.insert(20);
+    queue.insert(15);
+    queue.insert(25);
+    queue.insert(35);
+    queue.insert(17);
+
+    EXPECT_EQ(queue.extract(), 35);
+    EXPECT_EQ(queue.extract(), 25);
+    EXPECT_EQ(queue.extract(), 20);
+    EXPECT_EQ(queue.extract(), 17);
+    EXPECT_EQ(queue.extract(), 15);
+    EXPECT_EQ(queue.extract(), 10);
+    EXPECT_EQ(queue.extract(), 5);
+
+    EXPECT_TRUE(queue.isEmpty());
+}
+
+
+TEST(PriorityQueueTest, InsertAndExtractWithDuplicates) {
+    PriorityQueue queue;
+    queue.insert(10);
+    queue.insert(6);
+    queue.insert(5);
+    queue.insert(10);
+    queue.insert(6);
+
+    EXPECT_EQ(queue.extract(), 10);
+    EXPECT_EQ(queue.extract(), 10);
+    EXPECT_EQ(queue.extract(), 6);
+    EXPECT_EQ(queue.extract(), 6);
     EXPECT_EQ(queue.extract(), 5);
     EXPECT_TRUE(queue.isEmpty());
 }
 
-//≈щЄ тест на вставку и извлечение
-TEST(PriorityQueueTest, InsertAndExtract2) {
-    PriorityQueue pq;
-
-    pq.insert(10);
-    pq.insert(5);
-    pq.insert(20);
-    pq.insert(15);
-    pq.insert(25);
-
-    EXPECT_EQ(pq.extract(), 25);
-    EXPECT_EQ(pq.extract(), 20);
-    EXPECT_EQ(pq.extract(), 15);
-    EXPECT_EQ(pq.extract(), 10);
-    EXPECT_EQ(pq.extract(), 5);
-
-    EXPECT_TRUE(pq.isEmpty());
-}
-
-//тест на вставку и извлечение с одинаковыми элементами
-TEST(PriorityQueueTest, InsertAndExtractWithDuplicates) {
-    PriorityQueue pq;
-    pq.insert(10);
-    pq.insert(5);
-    pq.insert(10);
-    pq.insert(8);
-
-    EXPECT_EQ(pq.extract(), 10);
-    EXPECT_EQ(pq.extract(), 10);
-    EXPECT_EQ(pq.extract(), 8);
-    EXPECT_EQ(pq.extract(), 5);
-    EXPECT_TRUE(pq.isEmpty());
-}
-
-//тест на извлечение из пустой очереди
 TEST(PriorityQueueTest, ExtractFromEmptyQueue) {
-    PriorityQueue pq;
+    PriorityQueue queue;
 
-    EXPECT_THROW(pq.extract(), std::out_of_range);
+    EXPECT_THROW(queue.extract(), std::out_of_range);
 }
 
-//тест на вставку и извлечение с отрицательными элементами
+
 TEST(PriorityQueueTest, InsertWithNegativeNumbers) {
-    PriorityQueue pq;
+    PriorityQueue queue;
 
-    pq.insert(10);
-    pq.insert(-5);
-    pq.insert(25);
-    pq.insert(-10);
-    pq.insert(-2);
+    queue.insert(-34);
+    queue.insert(10);
+    queue.insert(-5);
+    queue.insert(25);
+    queue.insert(-7);
+    queue.insert(-2);
 
-    EXPECT_EQ(pq.extract(), 25);
-    EXPECT_EQ(pq.extract(), 10);
-    EXPECT_EQ(pq.extract(), -2);
-    EXPECT_EQ(pq.extract(), -5);
-    EXPECT_EQ(pq.extract(), -10);
-    EXPECT_TRUE(pq.isEmpty());
+    EXPECT_EQ(queue.extract(), 25);
+    EXPECT_EQ(queue.extract(), 10);
+    EXPECT_EQ(queue.extract(), -2);
+    EXPECT_EQ(queue.extract(), -5);
+    EXPECT_EQ(queue.extract(), -7);
+    EXPECT_EQ(queue.extract(), -34);
+    EXPECT_TRUE(queue.isEmpty());
 }
 
 int main(int argc, char** argv) {
