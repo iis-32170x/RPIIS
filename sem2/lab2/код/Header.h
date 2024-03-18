b538f236602a293e44e4e0b938c4d4735c7e7c65
ArTitoff <147609793+ArTitoff@users.noreply.github.com>
2024-03-18 20:06:40 +0300

    Add files via upload

---
+++ sem2/lab2/код/Header.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+struct Mnozh {
+    struct elem {
+        int kratn = 1;
+        std::string el;
+        int razm = 0;
+        bool NeorMN = false;
+    };
+    elem El[100];
+    int MoshMn = 1;
+    int KolMnoz;
+    int KolSkob;
+    int KolSkobKort;
+};
+
+
+void SortMnozh(Mnozh MultMn[]);
+void ViewMnozh(Mnozh MultMn[]);
+void SimRaznNM(Mnozh MultMn[]);
+void ReadFile(Mnozh MultMn[], std::string path);
+
