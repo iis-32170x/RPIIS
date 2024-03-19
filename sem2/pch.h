2df825fb2de193e5994b9a4eef7f2db626980394
nkzmch <146451109+nkzmch@users.noreply.github.com>
2024-03-19 16:03:42 +0300

    Update pch.h

--- sem2/pch.h
+++ sem2/pch.h
@@ -1,48 +1,37 @@
-﻿
-#ifndef PCH_H
+﻿#ifndef PCH_H
 #define PCH_H
 #include <iomanip>
 #include <iostream>
 
-class AVLTree {
-private:
-    struct Node {
-        int key;
-        Node* left;
-        Node* right;
-        int height;
-   
-        Node(int k) : key(k), left(nullptr), right(nullptr), height(1) {}
-    };
-
-    Node* root;
-
-    Node* insertNode(Node* node, int key);
-    Node* removeNode(Node* node, int key);
-    Node* findMinNode(Node* node);
-    Node* findMaxNode(Node* node);
-    Node* findClosestGreaterNode(Node* node, int key);
-    Node* findClosestSmallerNode(Node* node, int key);
-    int getHeight(Node* node);
-    int getBalanceFactor(Node* node);
-    void updateHeight(Node* node);
-    Node* rotateRight(Node* node);
-    Node* rotateLeft(Node* node);
-    Node* balance(Node* node);
-    
-public:
-    AVLTree();
-    ~AVLTree();
-    void clear(Node* node);
-    void insert(int key);
-    bool remove(int key);
-    bool search(int key);
-    int findMin();
-    int findMax();
-    int findClosestGreater(int key);
-    int findClosestSmaller(int key);
-    void print(); // Добавленная функция print
-    void print_n(const Node* p, int n, int level, int prob);
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+    int height;
 };
 
-#endif // PCH_H
+Node* createNode(int key);
+int getHeight(Node* node);
+int getBalanceFactor(Node* node);
+void updateHeight(Node* node);
+Node* rotateRight(Node* node);
+Node* rotateLeft(Node* node);
+Node* balance(Node* node);
+Node* insertNode(Node* node, int key);
+Node* removeNode(Node* node, int key);
+Node* findMinNode(Node* node);
+Node* findMaxNode(Node* node);
+Node* findClosestGreaterNode(Node* node, int key);
+Node* findClosestSmallerNode(Node* node, int key);
+void clear(Node* node);
+void insert(Node** root, int key);
+bool remove(Node** root, int key);
+bool search(Node* root, int key);
+int findMin(Node* root);
+int findMax(Node* root);
+int findClosestGreater(Node* root, int key);
+int findClosestSmaller(Node* root, int key);
+void print_n(const Node* p, int n, int level, int prob);
+void print(Node* root);
+
+#endif 
