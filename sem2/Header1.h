80c9c78037453577555e0e6a8c686f0e829a9627
pYro-sudo <109588143+pYro-sudo@users.noreply.github.com>
2024-03-24 13:16:51 +0300

    Update Header1.h

--- sem2/Header1.h
+++ sem2/Header1.h
@@ -2,7 +2,6 @@
 #pragma once
 
 struct JungTable {
-	int s = 3;
 
 	struct cell {
 		int val = -999;
@@ -13,6 +12,7 @@ struct JungTable {
 		cell* row = NULL;
 		tableRow* nextRow = NULL;
 	};
+
 
 	void Sort_p(cell** p) {
 		cell* t = NULL, * t1, * r;
@@ -29,7 +29,7 @@ struct JungTable {
 		} while ((*p)->nextCell->nextCell != t);
 	}
 
-	tableRow* createTable() {
+	tableRow* createRow() {
 		tableRow* t = new tableRow;
 		return t;
 	}
@@ -217,81 +217,87 @@ struct JungTable {
 	}
 
 	void addCell_New(tableRow* r, int val, int row) {
-		tableRow* t = r;
-		int i = 1;
-		while (i < row) {
-			if (t->nextRow) {
-				t = t->nextRow;
-				++i;
-			}
-			else {
-				break;
-			}
-		}
-
-		int k = 1;
-		int prev = 0;
-		tableRow* s = r;
-		while (k < row - 1) {
-			s = s->nextRow;
-			++k;
-		}
-		if (row - 1 <= 0) {
-			prev = 9999;
+		if (r->nextRow == NULL && r->row == NULL) {
+			r->row = createCell(val, r);
 		}
 		else {
-			prev = lengthOfRow(s);
-		}
-
-		if (val >= maximum(t->row, row)) {
-			cell* p = t->row;
-			while (p) {
-				if (p->nextCell == NULL && lengthOfRow(t) <= prev) {
-					p->nextCell = addCell(p, val);
-					break;
-				}
-				else if (p->nextCell == NULL && lengthOfRow(t) == prev) {
-					if (t->nextRow == NULL) {
-						t->nextRow = addRow(t);
-						t->nextRow->row = createCell(val, t->nextRow);
-						break;
-					}
-					else {
-						addCell_New(r, val, row + 1);
-						break;
-					}
-				}
-				else{
-					p = p->nextCell;
-				}
-			}
-		}
-		else{
-			cell* p = t->row;
-			while (p) {
-				if (p->nextCell && p->val < val && val < p->nextCell->val) {
-					int temp = p->nextCell->val;
-					p->nextCell->val = val;
-					if (t->nextRow == NULL) {
-						t->nextRow = addRow(t);
-						t->nextRow->row = createCell(temp, t->nextRow);
-						break;
-					}
-					else {
-						addCell_New(r, temp, row + 1);
-						break;
-					}
-				}
-				else if (!p->nextCell) {
-					int temp = p->val;
-					p->val = val;
-					p->nextCell = addCell(p, temp);
+			tableRow* t = r;
+			int i = 1;
+			while (i < row) {
+				if (t->nextRow) {
+					t = t->nextRow;
+					++i;
 				}
 				else {
-					p = p->nextCell;
+					break;
+				}
+			}
+
+			int k = 1;
+			int prev = 0;
+			tableRow* s = r;
+			while (k < row - 1) {
+				s = s->nextRow;
+				++k;
+			}
+			if (row - 1 <= 0) {
+				prev = 9999;
+			}
+			else {
+				prev = lengthOfRow(s);
+			}
+
+			if (val >= maximum(t->row, row)) {
+				cell* p = t->row;
+				while (p) {
+					if (p->nextCell == NULL && lengthOfRow(t) < prev) {
+						p->nextCell = addCell(p, val);
+						break;
+					}
+					else if (p->nextCell == NULL && lengthOfRow(t) == prev) {
+						if (t->nextRow == NULL) {
+							t->nextRow = addRow(t);
+							t->nextRow->row = createCell(val, t->nextRow);
+							break;
+						}
+						else {
+							addCell_New(r, val, row + 1);
+							break;
+						}
+					}
+					else {
+						p = p->nextCell;
+					}
+				}
+			}
+			else {
+				cell* p = t->row;
+				while (p) {
+					if (p->nextCell && p->val < val && val < p->nextCell->val) {
+						int temp = p->nextCell->val;
+						p->nextCell->val = val;
+						if (t->nextRow == NULL) {
+							t->nextRow = addRow(t);
+							t->nextRow->row = createCell(temp, t->nextRow);
+							break;
+						}
+						else {
+							addCell_New(r, temp, row + 1);
+							break;
+						}
+					}
+					else if (!p->nextCell) {
+						int temp = p->val;
+						p->val = val;
+						p->nextCell = addCell(p, temp);
+					}
+					else {
+						p = p->nextCell;
+					}
 				}
 			}
 		}
+		
 	}
 
 	void deleteTable(tableRow* r) {
