032a4474c82568687f054abdebd9ff11f8ec3ba0
Zubabubal <145017356+Zubabubal@users.noreply.github.com>
2024-03-27 19:50:21 +0300

    Update and rename sem2 to sem2Lab1Header.h

---
+++ sem2Lab1Header.h
@@ -0,0 +1,10 @@
+```
+#pragma once
+struct stack;
+void create(stack*& begin, stack*& end, int value);
+void add(stack*& b, stack*& e, int value);
+void view(stack* p);
+void del(stack*& p);
+void delStack(stack** p);
+void ftext();
+```
