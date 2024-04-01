080d8a8f9503d6235dca5c9f43f54726c19cb229
jakxcvv <147064507+jakxcvv@users.noreply.github.com>
2024-04-01 19:54:32 +0300

    Update main.cpp

--- sem2/lab1/code/main.cpp
+++ sem2/lab1/code/main.cpp
@@ -1,44 +1,62 @@
-#include "branch.h"
 #include <iostream>
+#include <string>
+#include "queue.h"
 
 using namespace std;
 
 int main() {
     setlocale(0, "");
-    Branch* rootA = nullptr;
-    Add(rootA, 5);
-    Add(rootA, 3);
-    Add(rootA, 8);
-    Add(rootA, 2);
-    Add(rootA, 4);
+    Node* front = nullptr;
+    Node* rear = nullptr;
 
-    Branch* rootB = nullptr;
-    Add(rootB, 8);
-    Add(rootB, 1);
-    Add(rootB, 4);
-    Add(rootB, 6);
-    Add(rootB, 9);
+    int choice;
+    string element;
 
-    Branch* unionResult = nullptr;
-    Union(rootA, rootB, unionResult);
-    cout << "Объединение: ";
-    print(unionResult);
-    cout << endl;
+    while (true) {
+        cout << "Меню:" << endl;
+        cout << "1) Добавить элемент" << endl;
+        cout << "2) Посмотреть очередь" << endl;
+        cout << "3) Взять элемент" << endl;
+        cout << "4) Очистка очереди" << endl;
+        cout << "5) Закрыть программу" << endl;
+        cout << "Выберите пункт меню: ";
+        cin >> choice;
 
-    Branch* intersectionResult = nullptr;
-    Intersection(rootA, rootB, intersectionResult);
-    cout << "Пересечение: ";
-    print(intersectionResult);
-    cout << endl;
-
-    removeNode(rootA, 3);
-    cout << "После удаления элемента: ";
-    print(rootA);
-    cout << endl;
-
-    Branch* foundElem = FindElem(rootA, 4);
-    if (foundElem) {
-        cout << "Найденный элемент: " << foundElem->data << endl;
+        switch (choice) {
+        case 1:
+            cout << "Введите элемент для добавления (q для возврата в меню): ";
+            cin.ignore();
+            getline(cin, element);
+            while (element != "q") {
+                enqueue(front, rear, element);
+                cout << "Введите следующий элемент (q для возврата в меню): ";
+                getline(cin, element);
+            }
+            break;
+        case 2:
+            printQueue(front);
+            break;
+        case 3:
+            if (!isEmpty(front)) {
+                string value = dequeue(front, rear);
+                if (value != "") {
+                    cout << "Взятый элемент: " << value << endl;
+                }                
+            }
+            else {
+                cout << "Очередь пуста." << endl;
+            }
+            break;
+        case 4:
+            clearQueue(front, rear);
+            break;
+        case 5:
+            cout << "Программа закрыта." << endl;
+            return 0;
+        default:
+            cout << "Некорректный выбор. Пожалуйста, выберите существующий пункт меню." << endl;
+            break;
+        }
     }
 
     return 0;
