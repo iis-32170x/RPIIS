22e9a2edf5166d453ff5f4f5a35c730f98e446d6
Hetman <danil46870@gmail.com>
2024-04-01 22:35:29 +0300

    Алгоритм исправлен, теперь всё красиво и рекурсивно в отличии от предыдущего неработающего (или работающего на 30%) merde

--- sem2/lab2/bulean.py
+++ sem2/lab2/bulean.py
@@ -8,6 +8,80 @@
 #   исходного множества и выводит его на экран. 
 ####################################################
 
+def readtuple(Tuple):
+    Elements = readElements(Tuple)
+    result = "<"
+    result += ','.join(Elements)
+    result += '>'
+    return result
+
+def readsubset(Set):
+    Elements = readElements(Set)
+    subset = "{"
+    subset += ','.join(Elements)
+    subset += '}'
+    return subset
+
+def readElements(Set):
+    Set = Set[1:-1]
+    isTuple = False
+    isSubset = False
+    subset = []
+    current_element = ""
+    for char in Set:
+        if ( char == ' '):
+            raise ValueError("Некорректная запись множества. Элементы множества не должны быть разделены пробелами.")
+        
+        if isSubset and current_element.count('{') == current_element.count('}'):
+            isSubset = False
+        elif isTuple and current_element.count('<') == current_element.count('>'):
+            isTuple = False
+
+        if char == ',' and not (isTuple or isSubset):
+            if (current_element.count('{') > 0):
+                if current_element[0] != '{' or current_element[-1] != '}' or current_element.count('{') != current_element.count('}'):
+                    raise ValueError("Некорректная запись элемента множества.")
+                subset.append(readsubset(current_element))
+                current_element = ""
+            elif current_element.count('<') > 0:
+                if current_element[0] != '<' or current_element[-1] != '>' or current_element.count('<') != current_element('>'):
+                    raise ValueError("Некорректная запись элемента множества.")
+                subset.append(readtuple(current_element))
+                current_element = ""
+            else:
+                subset.append(current_element)
+                current_element = ""
+
+        elif char == '<':
+            isTuple = True
+            current_element += char  
+        elif char == '{':
+            isSubset = True
+            current_element += char       
+        else:
+            current_element += char
+    
+    if current_element != "":
+        if current_element.count('{') != current_element.count('}') or current_element.count('<') != current_element.count('>'):
+            raise ValueError("Некорректная запись элемента множества.")
+        
+        if current_element.count('{') > 0:
+            if (current_element[0] != '{' or current_element[-1] != '}'):
+                raise ValueError("Некорректная запись элемента множества.")
+            subset.append(readsubset(current_element))
+            current_element = ""
+        elif current_element.count('<') > 0:
+            if (current_element[0] != '<' or current_element[-1] != '>'):
+                raise ValueError("Некорректная запись элемента множества.")
+            subset.append(readtuple(current_element))
+            current_element = ""
+
+        elif (subset.count(current_element) < 1):
+            subset.append(current_element)
+            current_element = ""
+
+    return subset
+
 def create_power_set(Set_elements):
     power_set = []
     for i in range (1 << len(Set_elements)):
@@ -18,86 +92,17 @@ def create_power_set(Set_elements):
         power_set.append(subset)
     return power_set
 
-def read_set(filename):
-    with open(filename, 'r') as file:
-        Set = file.readline().strip()
-        print(Set)
-        
-        if (Set[0] != '{' or Set[-1] != '}'):
-            raise ValueError("Множество не заключено в фигурные скобки.")
-        
-        Set_elements = [] #Множество строк, в которое будут записываться элементы исходного множества
-        current_element = "" #Переменная для хранения обрабатываемого элемента
-        Set = Set[1: -1]  #Обрезает крайние скобки у строки множества
-        i = 0
-        while(i < len(Set)):
-            if ( Set[i] == ' '):
-                raise ValueError("Некорректная запись множества. Элементы множества не должны быть разделены пробелами.")        
-            elif Set[i] == '{':                
-                isInside = True
-                current_element += Set[i]
-                i += 1
-
-                while i < len(Set):
-                    if Set[i] == ',' and not isInside:
-                        Set_elements.append(current_element)
-                        current_element = ""
-                        break
-                    elif Set[i] == ' ':
-                        raise ValueError("Некорректная запись множества. Элементы множества не должны быть разделены пробелами.")   
-                    current_element += Set[i] 
-                    
-                    if current_element.count('<') < current_element.count('>'):
-                        raise ValueError("Некорректная запись элемента множества")
-                            
-                    if current_element.count('{') == current_element.count('}'):
-                        if current_element[0] != '{' or current_element[-1] != '}':
-                            raise ValueError("Некорректная запись элемента множества")
-                        isInside = False    
-                    i += 1
-            elif Set[i] == '<':
-                isInside = True
-                current_element += Set[i]
-                i += 1
-                while i < len(Set):
-                    if Set[i] == ',' and not isInside:
-                        Set_elements.append(current_element)
-                        current_element = ""
-                        break
-                    if Set[i] == ' ':
-                        raise ValueError("Некорректная запись множества. Элементы множества не должны быть разделены пробелами.")   
-                    current_element += Set[i] 
-                    
-                    if current_element.count('{') < current_element.count('}'):
-                        raise ValueError("Некорректная запись элемента множества")
-                    
-                    elif current_element.count('<') == current_element.count('>'):
-                        if current_element[0] != '<' or current_element[-1] != '>':
-                            raise ValueError("Некорректная запись элемента множества")
-                        isInside = False
-                    i += 1
-
-            elif (Set[i] == ','): 
-                Set_elements.append(current_element)
-                current_element = ""
-            else:
-                current_element += Set[i]
-            i += 1
-        
-        if (current_element != ""):
-            if (current_element[0] == '{' and current_element[-1] != '}') or (current_element.count('{') != current_element.count('}')):
-                raise ValueError("Некорректная запись элемента множества")
-            if (current_element[0] == '<' and current_element[-1] != '>') or (current_element.count('<') != current_element.count('>')):
-                raise ValueError("Некорректная запись элемента множества")
-
-            Set_elements.append(current_element)
-            current_element = ""
-        return Set_elements
-
 filename = input("Введите имя файла: ")
 
-Set_elements = read_set(filename)
-power_set = create_power_set(Set_elements)
+with open(filename, 'r') as file:
+    SET = file.readline().strip()
+
+    print(SET)
+    print("-------------------------")
+    result = readElements(SET)
+    print(result)
+
+power_set = create_power_set(result)
 
 for i in range (len(power_set)):
     print(f"Подмножество {i+1}: {{{",".join(power_set[i])}}}")
