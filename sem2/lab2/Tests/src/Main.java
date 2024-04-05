30c546455423d81a8470e60d775a636c7a8d0536
Астахов Артём <artemastahov27@gmail.com>
2024-04-06 00:55:10 +0300

    Создал библиотеку

---
+++ sem2/lab2/Tests/src/Main.java
@@ -0,0 +1,11 @@
+import BooleanFounder.BooleanFounder;
+
+public class Main {
+    public static void main(String[] args) {
+        BooleanFounder booleanFounder = new BooleanFounder("src/Files/import.txt",
+                "src/Files/output.txt");
+
+        booleanFounder.getOriginalSet();
+        booleanFounder.findBoolean();
+    }
+}
