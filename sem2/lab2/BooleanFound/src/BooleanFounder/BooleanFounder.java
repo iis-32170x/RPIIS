b9ae17cff00a6c499a0ae750ff47a0926ae5fcc3
Астахов Артём <artemastahov27@gmail.com>
2024-04-06 00:40:37 +0300

    Добавил функцию проверки на корректоность данных

--- sem2/lab2/BooleanFound/src/BooleanFounder/BooleanFounder.java
+++ sem2/lab2/BooleanFound/src/BooleanFounder/BooleanFounder.java
@@ -8,6 +8,8 @@ public class BooleanFounder {
     private final ArrayList<String> set = new ArrayList<>();
     private final String inputFileName, outputFileName;
 
+    private boolean protectResult = true;
+
     public BooleanFounder(String inputFileName, String outputFileName) {
         this.inputFileName = inputFileName;
         this.outputFileName = outputFileName;
@@ -23,73 +25,119 @@ public class BooleanFounder {
             ex.getMessage();
         }
 
-        String temp = "";
-        boolean prot = false;
-        for (int i = 1; i < Objects.requireNonNull(fileSet).length() - 1; i++) {
+        assert fileSet != null;
+        if (!protectInputData(fileSet)) {
+            System.out.println("Данные введены некорректно");
+            protectResult = false;
+        } else {
+            String temp = "";
+            boolean prot = false;
+            for (int i = 1; i < Objects.requireNonNull(fileSet).length() - 1; i++) {
 
-            if (fileSet.charAt(i) == '<' || fileSet.charAt(i) == '{')
-                prot = true;
+                if (fileSet.charAt(i) == '<' || fileSet.charAt(i) == '{')
+                    prot = true;
 
-            if (fileSet.charAt(i) == '>' || fileSet.charAt(i) == '}')
-                prot = false;
+                if (fileSet.charAt(i) == '>' || fileSet.charAt(i) == '}')
+                    prot = false;
 
-            if (prot) {
-                temp += fileSet.charAt(i);
-                continue;
+                if (prot) {
+                    temp += fileSet.charAt(i);
+                    continue;
+                }
+
+                if (fileSet.charAt(i) != ',') {
+                    temp += fileSet.charAt(i);
+
+                } else {
+                    temp = temp.trim();
+                    set.add(temp);
+                    temp = "";
+                }
             }
-
-            if (fileSet.charAt(i) != ',') {
-                temp += fileSet.charAt(i);
-
-            } else {
-                temp = temp.trim();
-                set.add(temp);
-                temp = "";
+            temp = temp.trim();
+            set.add(temp);
+            if (!set.contains("")) {
+                set.add(0, "");
             }
-        }
-        temp = temp.trim();
-        set.add(temp);
-        if (!set.contains("")) {
-            set.add(0, "");
         }
     }
 
     public void findBoolean() {
-        ArrayList<String> resultSet = new ArrayList<>();
-        ArrayList<String> temp = new ArrayList<>();
+        if (protectResult) {
+            ArrayList<String> resultSet = new ArrayList<>();
+            ArrayList<String> temp = new ArrayList<>();
 
-        for (String subFromSet : set) {
-            if (resultSet.isEmpty())
-                resultSet.add(subFromSet);
-            else {
-                temp.clear();
-                temp.addAll(resultSet);
+            for (String subFromSet : set) {
+                if (resultSet.isEmpty())
+                    resultSet.add(subFromSet);
+                else {
+                    temp.clear();
+                    temp.addAll(resultSet);
 
-                for (String subFromResult : temp) {
-                    if (subFromResult.isEmpty() || subFromSet.isEmpty())
-                        resultSet.add(resultSet.size() - 1, subFromSet.concat(subFromResult));
-                    else
-                        resultSet.add(resultSet.size() - 1, subFromSet.concat(", ").concat(subFromResult));
+                    for (String subFromResult : temp) {
+                        if (subFromResult.isEmpty() || subFromSet.isEmpty())
+                            resultSet.add(resultSet.size() - 1, subFromSet.concat(subFromResult));
+                        else
+                            resultSet.add(resultSet.size() - 1, subFromSet.concat(", ").concat(subFromResult));
+                    }
                 }
             }
-        }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFileName))) {
-            bufferedWriter.flush();
-            bufferedWriter.write("{");
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFileName))) {
+                bufferedWriter.write("P(set) = {");
 
-            for (String s : resultSet) {
-                bufferedWriter.write("{");
-                bufferedWriter.write(s);
-                bufferedWriter.write("}");
-
-                if (resultSet.get(resultSet.size() - 1).equals(s))
+                for (String s : resultSet) {
+                    bufferedWriter.write("{");
+                    bufferedWriter.write(s);
                     bufferedWriter.write("}");
-                else
-                    bufferedWriter.write(", ");
+
+                    if (resultSet.get(resultSet.size() - 1).equals(s))
+                        bufferedWriter.write("}");
+                    else
+                        bufferedWriter.write(", ");
+                }
+            } catch (IOException ex) {
+                ex.getMessage();
             }
-        } catch (IOException ex) {
-            ex.getMessage();
+        } else {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFileName))) {
+                bufferedWriter.write("");
+                bufferedWriter.flush();
+            } catch (IOException ex) {
+                ex.getMessage();
+            }
         }
+    }
+
+    private boolean protectInputData(String fileSet) {
+        int count = 0;
+
+        if (fileSet.isEmpty() || fileSet.charAt(0) != '{' || fileSet.charAt(fileSet.length() - 1) != '}')
+            return false;
+        for (int i = 1; i < fileSet.length() - 1; i++) {
+            if (fileSet.charAt(i) == '<') {
+                count++;
+            }
+
+
+            if (fileSet.charAt(i) == '>') {
+                count--;
+            }
+        }
+        if (count != 0)
+            return false;
+
+
+        for (int i = 1; i < fileSet.length() - 1; i++) {
+            if (fileSet.charAt(i) == '{') {
+                count++;
+            }
+
+
+            if (fileSet.charAt(i) == '}') {
+                count--;
+            }
+        }
+        return count == 0;
     }
 }
