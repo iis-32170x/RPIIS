441c23a152672acc012d229d6253fa1a7bf8dcaa
jakxcvv <147064507+jakxcvv@users.noreply.github.com>
2024-04-21 12:42:52 +0300

    Update mnozh.h

--- sem2/lab2/code/mnozh.h
+++ sem2/lab2/code/mnozh.h
@@ -1,13 +1,11 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <vector>
-
 using namespace std;
 
 void cartesianProductRecursive(const vector<vector<string>>& sets, vector<vector<string>>& result, vector<string>& currentSet, int index);
 vector<vector<string>> cartesianProduct(const vector<vector<string>>& sets);
 void printSet(const vector<string>& set);
 vector<vector<int>> createSets(int num);
-
+void parseSet(const string& line, vector<string>& set);
 #endif
