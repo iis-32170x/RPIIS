16aa00c5ed514b3fd5d17682b593872600404e2a
Yudzhinious <144944000+Yudzhinious@users.noreply.github.com>
2024-05-06 19:36:26 +0300

    Create library.h

---
+++ sem 2/2 lab/library.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <conio.h>
+using namespace std;
+
+void Show(vector<string> reg);
+void File(vector<string>& count, ifstream& fin);
+void Union(vector<string>& count, string& obyedineniye);
