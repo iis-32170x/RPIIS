47ea53428040c800f87593e45eaba732ae8ee802
ILYApioivisMASTER <149104399+ILYApioivisMASTER@users.noreply.github.com>
2024-05-09 19:34:58 +0300

    Add files via upload

---
+++ sem2/labaPivo2/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "set.h"
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string file_path = "source.txt";
+    const auto result = intersection(file_path);
+    cout << "Пересечение  множеств: ";
+    cout << "{";
+    bool first = true;
+    for (const string& element : result) {
+        if (!first) {
+            cout << ", ";
+        }
+        cout << element;
+        first = false;
+    }
+    cout << "}";
+}
