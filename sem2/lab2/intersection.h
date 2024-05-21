041587708b5e452c4baf666c05a01b37b143e6b3
Nier Finik <139979165+Voerdo@users.noreply.github.com>
2024-05-21 09:41:35 +0300

    Update intersection.h

--- sem2/lab2/intersection.h
+++ sem2/lab2/intersection.h
@@ -3,8 +3,11 @@
 #include <fstream>;
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 string fromFile(string filename);
 void spacetest(string first_set, string second_set);
 string intersection(string first_set, string second_set);
-void set—hecking(string first_set, string second_set);
+void set–°hecking(string first_set, string second_set);
+void elementpush(string set, vector<string> vecSet);
+vector<string> multiple_occurrences(vector<string> set);
