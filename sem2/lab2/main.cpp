73b3dabaa1ff8082b31f7d4808179de39eac5f7b
PoznyakAnn <148707516+PoznyakAnn@users.noreply.github.com>
2024-05-22 19:41:53 +0300

    Add files via upload

---
+++ sem2/lab2/main.cpp
@@ -0,0 +1,23 @@
+﻿
+#include "pch.h"
+
+
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string file_path = "input.txt";
+    const auto result = union_sets(file_path);
+    cout << "Объединение множеств: ";
+    cout << "{";
+    bool first = true;
+    for (const string& element : result) {
+        if (!first) {
+            cout << ", ";
+        }
+        cout << element;
+        first = false;
+    }
+    cout << "}";
+}
