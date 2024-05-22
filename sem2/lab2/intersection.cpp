218c23c01519da33452fa373d6c88c6739a93de6
Nier Finik <139979165+Voerdo@users.noreply.github.com>
2024-05-22 22:22:29 +0300

    Update intersection.cpp

--- sem2/lab2/intersection.cpp
+++ sem2/lab2/intersection.cpp
@@ -1,4 +1,5 @@
 #include "intersection.h"
+
 string fromFile(string filename) {
 	string text;
 	ifstream file(filename);
@@ -12,112 +13,124 @@ string fromFile(string filename) {
 void spacetest(string first_set, string second_set) {
 	for (int i = 0; i < first_set.length(); i++) {
 		if (first_set[i] == ' ') {
-			cout << "Incorrect input" << endl;
-			cout << "Extra space in set 1";
-			exit(0);
+			first_set.erase(i, 1);
 		}
 	}
 	for (int i = 0; i < second_set.length(); i++) {
 		if (second_set[i] == ' ') {
-			cout << "Incorrect input" << endl;
-			cout << "Extra space in set 2";
-			exit(0);
+			second_set.erase(i, 1);
 		}
 	}
 }
-void setÑhecking(string first_set, string second_set) {
+
+void setСhecking(string first_set, string second_set) {
 	if (first_set[0] != '{' || first_set[first_set.length()-1] != '}') {
-		cout << "Set 1 is not one";
+		cout << "Set 1 is not a set";
 		exit(0);
 	}
 	if (second_set[0] != '{' || second_set[second_set.length()-1] != '}') {
-		cout << "Set 2 is not one";
+		cout << "Set 2 is not a set";
 		exit(0);
 	}
 }
-string intersection(string first_set, string second_set) {
-	string result = "{";
+
+vector<string> elementpush(string set) {
+	vector<string>vecSet;
 	int temp = 0;
 	string element = "";
-	vector<string> first_set_element;
-	vector<string> second_set_element;
-	for (int i = 1; i < first_set.length() - 1; i++) {
-		if (first_set[i] == '{' || first_set[i] == '<') {
+	for (int i = 1; i < set.length() - 1; i++) {
+		if (set[i] == '{' || set[i] == '<') {
 			temp++;
 		}
 		if (temp > 0) {
-			element += first_set[i];
+			element += set[i];
 		}
-		if (first_set[i] == '}' || first_set[i] == '>') {
+		if (set[i] == '}' || set[i] == '>') {
 			temp--;
 			if (temp == 0) {
-				first_set_element.push_back(element);
+				vecSet.push_back(element);
 				element = "";
 			}
 		}
-		if (temp == 0 && first_set[i] != ',' && first_set[i] != '}' && first_set[i] != '>') {
-			element += first_set[i];
+		if (temp == 0 && set[i] != ',' && set[i] != '}' && set[i] != '>') {
+			element += set[i];
 		}
-		if (temp == 0 && (first_set[i] == ',' || first_set[i] == '}' || first_set[i] == '>')) {
-			if (!element.empty()) {
-				first_set_element.push_back(element);
+		if (temp == 0 && (set[i] == ',' || set[i] == '}' || set[i] == '>')||(temp == 0 && i == set.length()-2)) {
+			if (!element.empty()) {																		
+				vecSet.push_back(element);                                              
 				element = "";
 			}
 		}
 		if (temp < 0) {
 			cout << "First set is incorrect, extra closing parenthesis" << endl;
-			cout << "Crossing is impossible" << endl;
+			cout << "Intersection is impossible" << endl;
 			exit(0);
 		}
-		if (i == first_set.length() - 1 && temp > 0) {
+		if (i == set.length() - 1 && temp > 0) {
 			cout << "First set is incorrect, extra opening parenthesis" << endl;
-			cout << "Crossing is impossible";
-			exit;
-		}
-	}
-
-	element = "";
-	for (int i = 1; i < second_set.length() - 1; i++) {
-		if (second_set[i] == '{' || second_set[i] == '<') {
-			temp++;
-		}
-		if (temp > 0) {
-			element += second_set[i];
-		}
-		if (second_set[i] == '}' || second_set[i] == '>') {
-			temp--;
-			if (temp == 0) {
-				second_set_element.push_back(element);
-				element = "";
-			}
-		}
-		if (temp == 0 && second_set[i] != ',' && second_set[i] != '}' && second_set[i] != '>') {
-			element += second_set[i];
-		}
-		if (temp == 0 && (second_set[i] == ',' || second_set[i] == '}' || second_set[i] == '>')) {
-			if (!element.empty()) {
-				second_set_element.push_back(element);
-				element = "";
-			}
-		}
-		if (temp < 0) {
-			cout << "Second set is incorrect, extra closing parenthesis" << endl;
-			cout << "Crossing is impossible" << endl;
+			cout << "Intersection is impossible";
 			exit(0);
 		}
-		if (i == second_set.length() - 1 && temp > 0) {
-			cout << "Second set is incorrect, extra opening parenthesis" << endl;
-			cout << "Crossing is impossible";
+		if ((set[i] == ',' && set[i + 1] == ',') || (set[i] == ',' && set[i + 1] == '}')) {
+			cout << "Incorrect set, extra ','";
+			exit(0);
 		}
 	}
-	for (int i = 0; i < first_set_element.size(); i++) {
-		for (int j = 0; j < second_set_element.size(); j++) {
-			if (first_set_element[i] == second_set_element[j]) {
-				result = result + first_set_element[i] + ',';
-			}
-		}
-	}
-	result.pop_back();
-	result += '}';
-	return result;
+	
+	return vecSet;
 }
+
+vector<string> multiple_occurrences(vector<string> set) {
+	string el;
+	string text_temp = "";
+	vector<string> current;
+	for (int i = 0; i < set.size(); i++) {
+		el = set[i];
+		if (el[0] == '{' && el[el.length()-1] == '}') { 
+			current = elementpush(el);
+			sort(current.begin(), current.end());
+			current.erase(unique(current.begin(), current.end()), current.end());
+			text_temp = "{";
+			for (int k = 0; k < current.size(); k++) {
+				text_temp += current[k];
+				if (k != current.size() - 1) {
+					text_temp += ",";
+				}
+			}
+			text_temp += "}";
+			set[i] = text_temp;
+		}
+	}
+	cout << "R: ";
+	for (int i = 0; i < set.size(); i++) {
+		cout << set[i] << endl;
+	}
+	return set;
+}
+
+string intersection(string first_set, string second_set) {
+	string result = "{";
+	vector<string> first_set_element;
+	vector<string> second_set_element;
+	vector<string> first_set_el_rework;
+	vector<string> second_set_el_rework;
+	first_set_element = elementpush(first_set);
+	first_set_element.erase(unique(first_set_element.begin(), first_set_element.end()), first_set_element.end());
+	second_set_element = elementpush(second_set);
+	second_set_element.erase(unique(second_set_element.begin(), second_set_element.end()), second_set_element.end());
+	first_set_el_rework = multiple_occurrences(first_set_element);
+	second_set_el_rework = multiple_occurrences(second_set_element);
+	cout << endl;
+	for (int i = 0; i < first_set_el_rework.size(); i++) {
+		for (int j = 0; j < second_set_el_rework.size(); j++) {
+				if (first_set_el_rework[i] == second_set_el_rework[j]) {
+					result = result + first_set_el_rework[i] + ',';
+				}
+			}   
+		} 
+		result.pop_back();
+		if (!result.empty()) {
+			result += '}';
+		}
+		return result;
+	}
