e7f091bba3d83da0cadc46dd126132930ffa4c3e
Yana <yanarumianseva.com>
2024-05-22 23:29:40 +0300

    lab2

--- Sem2/lab2/lab2.cpp
+++ Sem2/lab2/lab2.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 vector<string> Read(string path) {
-    ifstream f;
+    ifstream f; // Создаем объект для работы с файлом ввода
     f.open(path);
     if (!f.is_open()) {
         cout << "Error while opening file!" << endl;
@@ -15,15 +15,15 @@ vector<string> Read(string path) {
     vector<string> sets;
     string buff;
     while (getline(f, buff)) {
-        size_t found = buff.find('=');
+        size_t found = buff.find('='); // Ищем позицию символа '=' в текущей строке
         if (found != string::npos) {
-            string dataAfterEqualSign = buff.substr(found + 1);
+            string dataAfterEqualSign = buff.substr(found + 1); // Получаем подстроку, начиная с символа после '='
             sets.push_back(dataAfterEqualSign);
             cout << "Множество: " << dataAfterEqualSign << endl;
 
             int open = 0, close = 0;
-            for (char ch : dataAfterEqualSign) {
-                if (ch == '{' || ch == '<') open++;
+            for (char ch : dataAfterEqualSign) { // ch  поочередно содержать каждый символ из строки 'dataAfterEqualSign'
+                if (ch == '{' || ch == '<') open++;//  количество открывающих и закрывающих скобок
                 if (ch == '}' || ch == '>') close++;
             }
             if (open != close) {
@@ -37,7 +37,7 @@ vector<string> Read(string path) {
     return sets;
 }
 
-vector<string> Write(vector<string> sets, int& number) {
+vector<string> Write(vector<string> sets, int& number) { // вектор счетчика
     vector<string> FULLset(sets.size());
     for (int i = 0; i < sets.size(); i++) {
         FULLset[i] = sets[i];
@@ -48,30 +48,31 @@ vector<string> Write(vector<string> sets, int& number) {
 
 vector<string> Analys(const string& input) {
     vector<string> elements;
-    size_t pos = 1;
+    size_t pos = 1;// для отслеживания текущей позиции в строке
     string buffer;
-    int openBraces = 1;
-    for (; pos < input.length(); pos++) {
+    int openBraces = 1; // для подсчета открывающих скобок
+    for (; pos < input.length(); pos++) {     // итерация по каждому символу в строке input
         char c = input[pos];
         if (c == '{' || c == '<') {
             openBraces++;
-            if (openBraces == 2 && !buffer.empty()) {
-                elements.push_back(buffer);
+            if (openBraces == 2 && !buffer.empty()) { // если это вторая открывающая скобка и буфер не пустой
+                elements.push_back(buffer);  // добавить содержимое буфера в вектор элементов
                 buffer.clear();
             }
-            buffer += c;
+            buffer += c; //  текущий символ в буфер
         }
         else if (c == '}' || c == '>') {
             openBraces--;
             if (openBraces > 0) {
                 buffer += c;
             }
-            if (openBraces == 0) {
+            if (openBraces == 0) { //количество открывающих скобок стало равно 0 (закончился один элемент)
                 elements.push_back(buffer);
                 buffer.clear();
             }
         }
-        else if (openBraces == 1) {
+        else if (openBraces == 1) { //количество открывающих скобок равно 1 (находимся вне скобок)
+            
             if (c == ',') {
                 elements.push_back(buffer);
                 buffer.clear();
@@ -84,14 +85,14 @@ vector<string> Analys(const string& input) {
             buffer += c;
         }
     }
-    return elements;
+    return elements; // возвращаем вектор строк , содержащий разобранные элементы
 }
 
 vector<vector<string>> subsets(vector<string> sets) {
     vector<vector<string>> subsets;
-    for (int i = 0; i < sets.size(); i++) {
-        subsets.push_back(Analys(sets[i]));
-        for (const auto& element : subsets[i]) {
+    for (int i = 0; i < sets.size(); i++) {  // итерация по каждому множеству в переданном векторе sets
+        subsets.push_back(Analys(sets[i])); // функция Analys для разбора текущего множества, результат в вектор subsets
+        for (const auto& element : subsets[i]) { //  элементы подмножества на экран
             cout << element << " ";
         }
         cout << " - Элементы " << i + 1 << " множества." << endl;
@@ -101,19 +102,21 @@ vector<vector<string>> subsets(vector<string> sets) {
 
 string intersection(vector<string>& set1, vector<string>& set2) {
     string result;
-    bool isFirstElement = true;
+    bool isFirstElement = true; //   является ли текущий элемент первым в результирующей строке
 
-    for (auto it1 = set1.begin(); it1 != set1.end();) {
+    for (auto it1 = set1.begin(); it1 != set1.end();) { //от начала до конца множества 
         bool found = false;
-        for (auto it2 = set2.begin(); it2 != set2.end();) {
-            if (*it1 == *it2) {
-                if (!isFirstElement) {
+        bool found = false;
+        for (auto it2 = set2.begin(); it2 != set2.end();) { 
+            if (*it1 == *it2) { 
+                if (!isFirstElement) {  // если текущий элемент не первый в результирующей строке, добавляем перед ним запятую
+                   
                     result += ",";
                 }
                 result += *it1;
-                isFirstElement = false;
+                isFirstElement = false; //  флаг как false, так как первый элемент добавлен
 
-                it2 = set2.erase(it2);
+                it2 = set2.erase(it2); // удаление найденный элемент из второго множества
                 found = true;
                 break;
             }
@@ -138,21 +141,21 @@ string intersection(vector<string>& set1, vector<string>& set2) {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int sets_q = 0;
+    int sets_q = 0;  // для хранения количества множеств
     string path = "set.txt";
     auto Sets = Write(Read(path), sets_q);
-    auto Subsets = subsets(Sets);
+    auto Subsets = subsets(Sets); // функция subsets для создания подмножеств для каждого множества из вектора Sets
 
     if (Subsets.size() < 2) {
         cout << "Недостаточно множеств для нахождения пересечения!" << endl;
         return 0;
     }
 
-    string result = intersection(Subsets[0], Subsets[1]);
+    string result = intersection(Subsets[0], Subsets[1]); //  пересечение первых двух подмножеств
 
     for (int i = 2; i < sets_q; i++) {
-        auto help_intersec = Analys(result);
-        result = intersection(help_intersec, Subsets[i]);
+        auto help_intersec = Analys(result); // анализируем  результат на предмет совпадений и сохраняем его в переменную help_intersec
+        result = intersection(help_intersec, Subsets[i]);  //  пересечение текущего подмножества с предыдущим результатом и сохраняем результат в переменную result
     }
 
     cout << result << " - РЕЗУЛЬТАТ" << endl;
