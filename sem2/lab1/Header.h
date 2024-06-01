facdeb9abb5cb72628330f6ca4616c720161d2d5
ZArifv <150240210+ZArifv@users.noreply.github.com>
2024-06-01 18:47:33 +0300

    Create Header.h

---
+++ sem2/lab1/Header.h
@@ -0,0 +1,5 @@
+#pragma once
+void addFront(int*& arr, int& size, int& capacity, int element);
+void addBack(int*& arr, int& size, int& capacity, int element);
+void delfront(int*& arr, int& size);
+void delback(int*& arr, int& size);
