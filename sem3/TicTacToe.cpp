3441733581204c3a2a155d240c94a1b78e0caf3e
Daniil <dnk.semenov@gmail.com>
2024-09-17 20:01:08 +0300

    added comments to sources

--- sem3/TicTacToe.cpp
+++ sem3/TicTacToe.cpp
@@ -3,42 +3,81 @@
 #include <cctype>
 #include "TicTacToe.hpp"
 
-
+/*!
+ * \brief Конструктор по умолчанию.
+ *
+ * Инициализирует игровое поле стандартным размером 3x3.
+ */
 TicTacToe::TicTacToe() : board_size(3) {
     create_board(board_size);
 }
 
+/*!
+ * \brief Оператор доступа к строкам игрового поля.
+ *
+ * Позволяет получить доступ к строке игрового поля по индексу.
+ *
+ * \param i Индекс строки.
+ * \return Ссылка на вектор, представляющий строку игрового поля.
+ */
 std::vector<char> &TicTacToe::operator[](std::size_t i) {
     return board[i];
 }
 
-// метод для создания поля
+/*!
+ * \brief Метод для создания игрового поля.
+ *
+ * Создает игровое поле заданного размера и инициализирует его пустыми символами.
+ * Поле должно быть размером от 3 до 10.
+ *
+ * \param size Размер игрового поля.
+ */
 void TicTacToe::create_board(std::size_t size) {
     board_size = size;
-    if (board_size < 2 || board_size > 10) {
-        std::cout << "Неверный размер поля. Укажите размер >2 и <10" << std::endl;
-        return;
-    }
     board.clear();
     for (std::size_t i = 0; i < board_size; i++) {
         board.push_back(std::vector<char>(board_size, '-'));
     }
 }
 
+/*!
+ * \brief Получение текущего игрового поля.
+ *
+ * Возвращает текущее состояние игрового поля в виде двумерного вектора.
+ *
+ * \return Константная ссылка на игровое поле.
+ */
 const std::vector<std::vector<char>>& TicTacToe::get_board() {
     return board;
 }
 
-// метод для проверки валидности хода
+/*!
+ * \brief Проверка валидности хода.
+ *
+ * Проверяет, допустим ли ход в заданную ячейку поля. Ход считается валидным,
+ * если строка и колонка находятся в пределах игрового поля, и выбранная ячейка пуста.
+ *
+ * \param row Номер строки.
+ * \param col Номер колонки.
+ * \return true, если ход допустим, иначе false.
+ */
 bool TicTacToe::is_move_valid(int row, int col) {
-    if (row >= board_size || col >= board_size || board[row][col] != '-' ||
-        row < 0 || col < 0) {
+    if (row >= board_size || col >= board_size || board[row][col] != '-' || row < 0 || col < 0) {
         return false;
     }
     return true;
 }
 
-// метод для хода игрока
+/*!
+ * \brief Метод для выполнения хода игрока.
+ *
+ * Если ход допустим, устанавливает символ игрока в указанную ячейку.
+ *
+ * \param row Номер строки.
+ * \param col Номер колонки.
+ * \param player Символ игрока ('X' или 'O').
+ * \return true, если ход успешно выполнен, иначе false.
+ */
 bool TicTacToe::make_move(int row, int col, char &player) {
     if (!is_move_valid(row, col)) {
         return false;
@@ -47,7 +86,14 @@ bool TicTacToe::make_move(int row, int col, char &player) {
     return true;
 }
 
-// метод для проверки победителя
+/*!
+ * \brief Проверка победителя.
+ *
+ * Проверяет, победил ли указанный игрок, проверяя строки, столбцы и диагонали на наличие одинаковых символов.
+ *
+ * \param player Символ игрока ('X' или 'O').
+ * \return true, если игрок победил, иначе false.
+ */
 bool TicTacToe::check_winner(char player) {
     // проверка горизонталей
     for (std::size_t i = 0; i < board.size(); i++) {
@@ -104,6 +150,13 @@ bool TicTacToe::check_winner(char player) {
     return false;
 }
 
+/*!
+ * \brief Получить количество сделанных ходов.
+ *
+ * Подсчитывает количество ходов на игровом поле, то есть количество непустых ячеек.
+ *
+ * \return Количество ходов.
+ */
 int TicTacToe::moves_count() {
     int count = 0;
     for (std::size_t i = 0; i < board_size; i++) {
@@ -116,6 +169,13 @@ int TicTacToe::moves_count() {
     return count;
 }
 
+/*!
+ * \brief Получить размер игрового поля.
+ *
+ * Возвращает текущий размер игрового поля.
+ *
+ * \return Размер игрового поля.
+ */
 std::size_t TicTacToe::get_board_size() {
     return board_size;
 }
