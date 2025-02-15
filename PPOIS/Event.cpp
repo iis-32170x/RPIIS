ce13a730564e10713e2f971e45dc06a0cf865000
enterpraizer <148863144+enterpraizer@users.noreply.github.com>
2025-02-15 12:49:05 +0300

    Delete PPOIS directory

--- PPOIS/Event.cpp
+++
@@ -1,4 +0,0 @@
-#include "Event.h"
-
-Event::Event(const std::string& eventName, const std::string& eventDate)
-    : eventName(eventName), eventDate(eventDate) {}
--- PPOIS/Staff.cpp
+++
@@ -1,4 +0,0 @@
-#include "Staff.h"
-
-Staff::Staff(const std::string& name, const std::string& role)
-    : name(name), role(role) {}
