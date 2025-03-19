a15dcce6b13debfb733c444d8c5696830b591918
enterpraizer <148863144+enterpraizer@users.noreply.github.com>
2025-03-19 12:46:46 +0300

    Delete PPOIS_LAB3/PPOIS_LAB3 directory

--- PPOIS_LAB3/PPOIS_LAB3/fonts.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-#pygame.init()
-
-font = pygame.font.Font(None, 15)
-font_menu=pygame.font.Font("images/ofont.ru_Wayward.ttf", 50)
-font_lose=pygame.font.Font("images/ofont.ru_Ghastly Panic.ttf", 200)
-font_win = pygame.font.Font("images/ofont.ru_Art Nouveau-Bistro.ttf", 125)
